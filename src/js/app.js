273b20dbbd7a1be5a9d07eb2bb0c7b564ef282cd
Nick Snyder <n.a.snyder@comcast.net>
2018-03-31 15:23:53 -0400

    Added ability to add new weather tab

--- src/js/app.js
+++ src/js/app.js
@@ -7,16 +7,36 @@ import 'materialize-css/dist/css/materialize.css';
 import '../assets/css/style.css';
 // import 'material-design-icons/iconfont/material-icons.css';
 
-let weatherTabs;
+var weatherTabs;
+var addModal;
 
 
 // EVENT LISTENERS
-document.addEventListener('DOMContentLoaded', initMaterialize);
-document.addEventListener('DOMContentLoaded', getWeather('Flemington', 'NJ'));
+// DOM loaded event listener
+document.addEventListener('DOMContentLoaded', () => {
+  initMaterialize();
+  getWeather(document.querySelector('#weatherTabs').children[0].children[0].attributes.href.value);
+});
+// Weather tab clicked event listener
+document.querySelector('#weatherTabs').addEventListener('click', tabClicked);
+// Add weather tab event listener
+document.querySelector('#addTab').addEventListener('click', showAddModal);
+// Confirm add weather
+document.querySelector('#confirmWeatherBtn').addEventListener('click', addWeatherTab);
+
+
 
 // Initialize Materialize elements
 function initMaterialize(){
-  const tabs = document.querySelector('#weatherTabs');
+  initTabs();
+
+  initModal();
+}
+
+// Initialize tabs
+function initTabs(){
+  const tabs = document.querySelector('.tabs');
+
   weatherTabs = M.Tabs.init(tabs, {
     swipeable: true,
     duration: 300,
@@ -25,11 +45,38 @@ function initMaterialize(){
   });
 }
 
-function getWeather(city, state){
-  weather.getWeather(city, state)
+// Initialize Modal
+function initModal(){
+  var modal = document.querySelector('.modal');
+  addModal = M.Modal.init(modal, {});
+}
+
+// Show add tab modal
+function showAddModal(){
+  addModal.open();
+}
+
+// Add new weather tab to tabs
+function addWeatherTab(){
+  ui.addNewWeatherTab((city, state) => {
+    weatherTabs.destroy();
+    initTabs();
+    weatherTabs.select(`${city}_${state}`);
+  });
+}
+
+// Weather tab clicked
+function tabClicked(e){
+  if(e.target.classList.contains('tabLink')){
+    getWeather(e.target.attributes.href.value);
+  }
+}
+
+// Get weather
+function getWeather(targetTabID){
+  weather.getWeather('Flemington', 'NJ')
     .then(results => {
-      console.log(weatherTabs);
-      ui.fillTab(results);
+      ui.fillTab(results, targetTabID);
     })
     .catch(err => console.log(err));
 }
--- src/js/ui.js
+++ src/js/ui.js
@@ -1,14 +1,59 @@
 class UI {
   constructor(){
-
+    this.tabsList = document.querySelector('#weatherTabs');
+    this.weatherCards = document.querySelector('#weatherCards');
   }
 
-  //Fill weather tab
-  fillTab(weather){
-    
+  // Fill weather tab
+  fillTab(weather, targetTabID){
+    console.log(weather);
+    const targetTab = document.querySelector(targetTabID);
+    targetTab.querySelector('.location').textContent = weather.display_location.full;
+    targetTab.querySelector('.w-overview').textContent = weather.icon;
+    targetTab.querySelector('.temp').textContent = weather.temperature_string;
   }
-  //Show weather
 
+  // Add new weather tab
+  addNewWeatherTab(callback){
+    const city = document.querySelector('#newCity').value;
+    const state = document.querySelector('#newState').value;
+    const newTab = document.createElement('li');
+    newTab.className = 'tab col s3';
+    newTab.innerHTML = `
+      <a class="tabLink" href="#${city}_${state}">${city}, ${state}</a>
+    `;
+    this.tabsList.appendChild(newTab);
+
+    const newCard = document.createElement('div');
+    newCard.className = 'card blue-grey darken-1';
+    newCard.id = `${city}_${state}`;
+    newCard.innerHTML = `
+      <div class="card-content white-text center-align" style="background-image: url(https://cdn.shopify.com/s/files/1/1205/5946/products/Epic_Rain_Thunder_1024x1024.jpg?v=1497034556);">
+        <h1 class="red-text"><span class="location">LOCATION</span></h1>
+        <h4 class="red-text wOverview"><span class="w-overview">Weather Overview</span></h4>
+        <p class="red-text wTemp">TEMP: <span class="temp">F/C</span></p>
+        <a class="waves-effect activator btn">More...</a>
+      </div>
+      <div class="card-reveal">
+        <span class="card-title grey-text text-darken-4"><span class="reveal-location">LOCATION</span><i class="material-icons right">close</i></span>
+        <ul class="collection">
+          <li class="collection-item">TEMP: <span class="reveal-temp">F/C</span></li>
+          <li class="collection-item"><span class="wind">Wind</span></li>
+          <li class="collection-item"><span class="humidity">Humidity</span></li>
+          <li class="collection-item"><span class="pressure">Pressure</span></li>
+          <li class="collection-item"><span class="wind-chill">Wind Chill</span></li>
+        </ul>
+      </div>
+    `;
+    this.weatherCards.appendChild(newCard);
+    callback(city, state);
+  }
+
+  // Clear add modal inputs
+  clearAddFields(){
+    document.querySelector('#newCity').value = '';
+    document.querySelector('#newState').value = '';
+  }
 }
 
 export const ui = new UI();
