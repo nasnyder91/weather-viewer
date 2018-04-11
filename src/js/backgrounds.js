42ff9b0561057392b2905a1d4cefd25d8f5e9176
Nick Snyder <n.a.snyder@comcast.net>
2018-04-11 19:43:07 -0400

    Added images Reduced image sizes

--- src/js/backgrounds.js
+++ src/js/backgrounds.js
@@ -1,14 +1,17 @@
-import rain from '../assets/img/rain.jpeg';
-import snow from '../assets/img/snow.jpeg';
-import mist from '../assets/img/mist.jpeg';
-import fog from '../assets/img/fog.jpeg';
-import haze from '../assets/img/haze.jpeg';
-import sandstorm from '../assets/img/sandstorm.jpeg';
-import storm from '../assets/img/storm.jpeg';
-import clear from '../assets/img/clear.jpeg';
-import partlycloudy from '../assets/img/partlycloudy.jpeg';
-import cloudy from '../assets/img/cloudy.jpeg';
-import ice from '../assets/img/ice.jpeg';
+import rain from '../assets/img/rain.jpg';
+import snow from '../assets/img/snow.jpg';
+import mist from '../assets/img/mist.jpg';
+import fog from '../assets/img/fog.jpg';
+import haze from '../assets/img/haze.jpg';
+import sandstorm from '../assets/img/sandstorm.jpg';
+import storm from '../assets/img/storm.jpg';
+import clear from '../assets/img/clear.jpg';
+import partlycloudy from '../assets/img/partlycloudy.jpg';
+import cloudy from '../assets/img/cloudy.jpg';
+import ice from '../assets/img/ice.jpg';
+import dust from '../assets/img/dust.jpg';
+import smoke from '../assets/img/smoke.jpg';
+import tornado from '../assets/img/funnelCloud.jpg';
 
 
 
@@ -30,30 +33,30 @@ class Backgrounds {
       case 'snow grains':bg = snow;break;
       case 'ice crystals':bg = ice;break;
       case 'ice pellets':bg = ice;break;
-      case 'hail':bg = '';break;
+      case 'hail':bg = ice;break;
       case 'mist':bg = mist;break;
       case 'fog':bg = fog;break;
       case 'fog patches':bg = fog;break;
-      case 'smoke':bg = '';break;
-      case 'volcanic ash':bg = '';break;
-      case 'widespread dust':bg = '';break;
-      case 'sand':bg = '';break;
+      case 'smoke':bg = smoke;break;
+      case 'volcanic ash':bg = smoke;break;
+      case 'widespread dust':bg = dust;break;
+      case 'sand':bg = sandstorm;break;
       case 'haze':bg = haze;break;
-      case 'spray':bg = '';break;
-      case 'dust whirls':bg = '';break;
+      case 'spray':bg = mist;break;
+      case 'dust whirls':bg = dust;break;
       case 'sandstorm':bg = sandstorm;break;
       case 'low drifting snow':bg = snow;break;
-      case 'low drifting widespread dust':bg = '';break;
+      case 'low drifting widespread dust':bg = dust;break;
       case 'low drifting sand':bg = sandstorm;break;
       case 'blowing snow':bg = snow;break;
-      case 'blowing widespread dust':bg = '';break;
+      case 'blowing widespread dust':bg = dust;break;
       case 'blowing sand':bg = sandstorm;break;
       case 'rain mist':bg = mist;break;
       case 'rain showers':bg = rain;break;
       case 'snow showers':bg = snow;break;
       case 'snow blowing snow mist':bg = snow;break;
       case 'ice pellet showers':bg = ice;break;
-      case 'hail showers':bg = '';break;
+      case 'hail showers':bg = 'ice';break;
       case 'thunderstorm':bg = storm;break;
       case 'thunderstorms and rain':bg = storm;break;
       case 'thunderstorms and snow':bg = storm;break;
@@ -71,12 +74,12 @@ class Backgrounds {
       case 'partly cloudy':bg = partlycloudy;break;
       case 'mostly cloudy':bg = cloudy;break;
       case 'scattered clouds':bg = partlycloudy;break;
-      case 'small hail':bg = '';break;
+      case 'small hail':bg = ice;break;
       case 'squalls':bg = storm;break;
-      case 'funnel cloud':bg = '';break;
+      case 'funnel cloud':bg = tornado;break;
       case 'unknown precipitation':bg = '';break;
       case 'unknown':bg = '';break;
-      default: bg = '';
+      default: bg = clear;
     }
 
     return bg;
